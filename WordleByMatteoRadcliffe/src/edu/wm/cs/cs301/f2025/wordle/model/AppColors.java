ff1ecf8d5f7daa0c86e4b739a4c04a4cab0f979d
Matteo Radcliffe <mdradcliffe@wm.edu>
2025-10-04 19:38:55 -0400

    AppColors documentation done with the help of AI

--- WordleByMatteoRadcliffe/src/edu/wm/cs/cs301/f2025/wordle/model/AppColors.java
+++ WordleByMatteoRadcliffe/src/edu/wm/cs/cs301/f2025/wordle/model/AppColors.java
@@ -1,13 +1,20 @@
 package edu.wm.cs.cs301.f2025.wordle.model;
 
 import java.awt.Color;
-
+/**
+ * The AppColors class represents the AppColors component of the Wordle application.
+ * It is responsible for handling its respective UI or logic functionality within the game.
+ */
 public class AppColors {
 	
+	// Construct a new object—initialize and configure it close to creation for readability.
 	public static Color GRAY = new Color(120, 124, 126);
+	// Construct a new object—initialize and configure it close to creation for readability.
 	public static Color GREEN = new Color(106, 170, 100);
+	// Construct a new object—initialize and configure it close to creation for readability.
 	public static Color YELLOW = new Color(201, 180, 88);
 	
+	// Construct a new object—initialize and configure it close to creation for readability.
 	public static Color OUTLINE = new Color(211, 214, 218);
 
 }
