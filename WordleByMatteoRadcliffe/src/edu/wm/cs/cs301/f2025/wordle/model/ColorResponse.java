3d102f17f6e51ae1c87e02901320c3774f7f40c4
Matteo Radcliffe <mdradcliffe@wm.edu>
2025-10-04 19:41:05 -0400

    ColorResponse documentation done with the help of AI

--- WordleByMatteoRadcliffe/src/edu/wm/cs/cs301/f2025/wordle/model/ColorResponse.java
+++ WordleByMatteoRadcliffe/src/edu/wm/cs/cs301/f2025/wordle/model/ColorResponse.java
@@ -2,6 +2,10 @@ package edu.wm.cs.cs301.f2025.wordle.model;
 
 import java.awt.Color;
 
+/**
+ * The ColorResponse class represents the ColorResponse component of the Wordle application.
+ * It is responsible for handling its respective UI or logic functionality within the game.
+ */
 public class ColorResponse {
 	
 	private final Color backgroundColor, foregroundColor;
@@ -11,10 +15,18 @@ public class ColorResponse {
 		this.foregroundColor = foregroundColor;
 	}
 
+    /**
+     * getBackgroundColor method performs its core logic or handles UI actions as defined.
+     * @return result of the operation
+     */
 	public Color getBackgroundColor() {
 		return backgroundColor;
 	}
 
+    /**
+     * getForegroundColor method performs its core logic or handles UI actions as defined.
+     * @return result of the operation
+     */
 	public Color getForegroundColor() {
 		return foregroundColor;
 	}
