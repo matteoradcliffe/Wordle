0d3ad270f3fbbf470c9105ea47ab203cc584e2f1
Matteo Radcliffe <mdradcliffe@wm.edu>
2025-11-02 14:58:41 -0500

    Added first two methods. initialize() and setWordList() to AbstractModel

--- WordleByMatteoRadcliffe/src/edu/wm/cs/cs301/f2025/wordle/model/AbstractModel.java
+++ WordleByMatteoRadcliffe/src/edu/wm/cs/cs301/f2025/wordle/model/AbstractModel.java
@@ -7,11 +7,26 @@ public abstract class AbstractModel implements Model{
 	protected final int columnCount = 5;
 	protected final int maximumRows = 6;
 	protected int currentColumn = -1;
-	protected int curentRow = 0;
+	protected int currentRow = 0;
 	
 	protected char[] guess = new char[columnCount];
 	protected WordleResponse[][] grid = new WordleResponse[maximumRows][columnCount];
 	
-	protected List<String> wordlist = new ArrayList<>();
+	protected List<String> wordList = new ArrayList<>();
 	protected final Statistics statistics = new Statistics();
+	
+	@Override
+	public void initialize() {
+		grid = new WordleResponse[maximumRows][columnCount];
+		currentColumn = -1;
+		currentRow = 0;
+		guess = new char[columnCount];
+		generateCurrentWord();
+	}
+	
+	@Override
+	public void setWordList(List<String> list) {
+		wordList = list;
+	}
+	
 }
